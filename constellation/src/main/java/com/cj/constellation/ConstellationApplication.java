053c98372353f35ff6388626205400842f8f8744
Chetan Chilhate <chilhatechetan04@gmail.com>
2021-09-23 10:14:58 +0530

    enable eureka client

--- constellation/src/main/java/com/cj/constellation/ConstellationApplication.java
+++ constellation/src/main/java/com/cj/constellation/ConstellationApplication.java
@@ -2,8 +2,10 @@ package com.cj.constellation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ConstellationApplication {
 
 	public static void main(String[] args) {
