9a809ff2e7ee6e6e581af86b3df59f96a456ef24
Chetan Chilhate <chilhatechetan04@gmail.com>
2021-11-20 21:00:36 +0530

    update department id genration stratergy

--- department-service/src/main/java/com/cj/department/entity/Department.java
+++ department-service/src/main/java/com/cj/department/entity/Department.java
@@ -4,19 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "t_department")
 public class Department {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long departmentId;
     private String departmentName;
     private String departmentAddress;
