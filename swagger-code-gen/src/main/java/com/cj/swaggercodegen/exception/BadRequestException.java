e12a868f0e659c6f388082011e28b8f7a24ba5f6
Chetan Chilhate <chilhatechetan04@gmail.com>
2021-11-27 09:24:31 +0530

    add bad request exception

---
+++ swagger-code-gen/src/main/java/com/cj/swaggercodegen/exception/BadRequestException.java
@@ -0,0 +1,9 @@
+package com.cj.swaggercodegen.exception;
+
+public class BadRequestException extends RuntimeException {
+
+  public BadRequestException(String message) {
+    super(message);
+  }
+
+}
